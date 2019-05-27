f451b0a75e7c781dcf52a08482704e3065351523
Anid Murmu <anidmurmu@gmail.com>
2019-05-27 22:29:47 +0530

    chapter 24 challenge 1 complete, use gson to deserialize from json

---
+++ Challenges/PhotoGalleryCh25_1/app/src/main/java/com/bignerdranch/android/photogallery/Photos.java
@@ -0,0 +1,137 @@
+package com.bignerdranch.android.photogallery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Photos {
+    int page;
+    int pages;
+    int perPage;
+    int total;
+    Photo photo;
+    List<Photo> mPhotoList = new ArrayList<>();
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(int perPage) {
+        this.perPage = perPage;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+}
+
+class Photo {
+    String id;
+    String owner;
+    String secret;
+    String server;
+    int farm;
+    String title;
+    int isPublic;
+    int isFriend;
+    int isFamily;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public int getFarm() {
+        return farm;
+    }
+
+    public void setFarm(int farm) {
+        this.farm = farm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(int isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public int getIsFriend() {
+        return isFriend;
+    }
+
+    public void setIsFriend(int isFriend) {
+        this.isFriend = isFriend;
+    }
+
+    public int getIsFamily() {
+        return isFamily;
+    }
+
+    public void setIsFamily(int isFamily) {
+        this.isFamily = isFamily;
+    }
+}
