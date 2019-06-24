d1a0320865c1337f33dcfaf512fd138c16f26454
Anid Murmu <anid.murmu@capfront.in>
2019-06-24 19:25:35 +0530

    in progress

--- Challenges/Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
+++ Challenges/Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
@@ -1,13 +1,39 @@
 package com.bignerdranch.android.locatr;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.support.v4.app.Fragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
-public class LocatrActivity extends AppCompatActivity {
+public class LocatrActivity extends SingleFragmentActivity {
+  public static final int REQUEST_ERROR = 0;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  protected Fragment createFragment() {
+    return LocatrFragment.newInstance();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+    int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+    if (errorCode != ConnectionResult.SUCCESS) {
+      Dialog errorDialog = apiAvailability
+          .getErrorDialog(this, errorCode, REQUEST_ERROR,
+              new OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                  // Leave if services are unavailable
+                  finish();
+                }
+              });
+
+      errorDialog.show();
+    }
   }
 }
